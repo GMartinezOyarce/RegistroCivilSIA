d00dc7a5439ebf5d15bdccb717eb3c5e2008089a
Gabriel <gabriel.martinez.o@mail.pucv.cl>
2025-08-19 21:25:13 -0400

    cambio de nombre(Nacimiento) se Mantienen buenas practicas

--- src/main/java/cl/pucv/inf2236/registrocivilsia/modelo/Nacimiento.java
+++ src/main/java/cl/pucv/inf2236/registrocivilsia/modelo/Nacimiento.java
@@ -14,16 +14,36 @@ public class Nacimiento {
     private int idActa;
     private LocalDate fechaInscripcion;
     private String lugarNacimiento;
+    
     private Persona inscrito;
     private Persona progenitor1;
     private Persona progenitor2;
     
-    public Nacimiento(int idActa, String lugarNacimiento, Persona inscrito, Persona progenitor1, Persona progenitor2){
-        this.idActa = idActa;
-        this.lugarNacimiento = lugarNacimiento;
-        this.inscrito = inscrito;
+    //getters
+    public int getIdActa(){
+        return this.idActa;
+    }
+    public LocalDate getFechaInscripcion(){
+        return this.fechaInscripcion;
+    }
+    public String getLugarNacimiento(){
+        return this.lugarNacimiento;
+    }
+    public Persona getInscrito(){
+        return this.inscrito;
+    }
+    public Persona getProgenitor1(){
+        return this.progenitor1;
+    }
+    public Persona getProgenitor2(){
+        return this.progenitor2;
+    }
+    
+    //setters
+    public void setProgenitor1(Persona progenitor1){
         this.progenitor1 = progenitor1;
+    }
+    public void setProgenitor2(Persona progenitor2){
         this.progenitor2 = progenitor2;
-        this.fechaInscripcion = LocalDate.now();
     }
 }

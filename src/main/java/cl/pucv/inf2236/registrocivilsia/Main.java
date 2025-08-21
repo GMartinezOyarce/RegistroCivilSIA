d596592155f18a0ef373affb28310adb67f787f0
Gabriel <gabriel.martinez.o@mail.pucv.cl>
2025-08-20 23:07:30 -0400

    Se agrega el inicio del main

--- src/main/java/cl/pucv/inf2236/registrocivilsia/Main.java
+++ src/main/java/cl/pucv/inf2236/registrocivilsia/Main.java
@@ -4,13 +4,42 @@
 
 package cl.pucv.inf2236.registrocivilsia;
 
+//importacion de clases
+import cl.pucv.inf2236.registrocivilsia.modelo.Sucursal;
+import cl.pucv.inf2236.registrocivilsia.modelo.logica.SistemaRegistroCivil;
+
+//importacion de librerias
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
 /**
  *
  * @author gabom
  */
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        //inicializar lector y variable para leer datos;
+        BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+        String dato;
+        
+        //cargar Datos
+        SistemaRegistroCivil sistema = new SistemaRegistroCivil();
+        sistema.cargarDatosDePrueba();
+        
+        //variable booleana para poder terminar la ejecucion del programa
+        boolean salir = false;
+        
+        System.out.println("---Bienvenido al sistema del registro civil---");
+        
+        while(!salir){   
+            System.out.println("--- Eliga la sucursal a la cual desea ingresar ---");
+            List<Sucursal> sucursales = sistema.getListaSucursales();
+            for(int i = 0; i< sucursales.size(); i++){
+                System.out.println((1 + i) + "." + sucursales.get(i).getNombre());
+            }
+        }    
     }
 }

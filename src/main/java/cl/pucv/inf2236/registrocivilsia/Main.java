211890ef75287928e42e0ebc1305b7d68a55c0fe
osval <osval@Claudio>
2025-08-27 23:33:55 -0400

    Modificacion General(Registro de Matrimonio, Registro Defuncion[por terminar])

--- src/main/java/cl/pucv/inf2236/registrocivilsia/Main.java
+++ src/main/java/cl/pucv/inf2236/registrocivilsia/Main.java
@@ -23,10 +23,22 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         //variables que use (no sabia donde dejarlas) ATTE: cris
-        int index, año, mes, dia;
+        int index = 0, año, mes, dia;
+        
+        //variables Nacimiento ATTE: cris
         LocalDate fechaInscripcion, fechaNacimiento;
-        String lugarNacimiento, rutProgenitor1, rutProgenitor2, rutInscrito, nombre;        
-
+        String lugarNacimiento, rutProgenitor1, rutProgenitor2, rutInscrito, nombre;    
+        
+        
+        //variables Matrimonio ATTE: yayo
+        LocalDate fechaMatrimonio;
+        String rutConyuge1, rutConyuge2;
+        
+        //variables Defuncion ATTE: yayo
+        LocalDate fechaDefuncion;
+        String rutFallecido, Causa;
+        
+        
         //inicializar lector y variable para leer datos;
         BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
         //String dato;
@@ -99,9 +111,55 @@ public class Main {
                     break;
                 case 2:
                     //manejarRegistroMatrimonio(scanner, sistema);
+                    
+                    //Creo que poner la sucursal primero es bueno por lo que lo copie
+                    System.out.println("Seleccione Sucursal:");
+                    
+                    for(int i=0; i<sistema.getListaSucursales().size(); i++){
+                        System.out.println((i+1) + ". " + sistema.getListaSucursales().get(i).getNombre());
+                    }
+                    index = Integer.parseInt(lector.readLine());
+                    index--;
+                    
+                    //Fecha del Matrimonio
+                    
+                    System.out.println("Ingrese año del matrimonio:");
+                    año = Integer.parseInt(lector.readLine());
+                    System.out.println("Ingrese mes del matrimonio:");
+                    mes = Integer.parseInt(lector.readLine());
+                    System.out.println("Ingrese dia del matrimonio::");
+                    dia = Integer.parseInt(lector.readLine());
+                    fechaMatrimonio = LocalDate.of(año,mes,dia);
+                    //me falta hacer el cambio en el archivo de matriomnio para que la fecha se llene desde aqui nomas
+
+                    //Rut de las Personas
+                    System.out.println("Ingrese el rut del primer Conyuge del Matrimonio:");
+                    rutConyuge1 = lector.readLine();
+                    System.out.println("Ingrese el rut del segundo Conyuge del Matrimonio:");
+                    rutConyuge2 = lector.readLine();
+                    
+                    //Ingreso de Datos
+                    sistema.registrarMatrimonio(index, fechaMatrimonio, rutConyuge1, rutConyuge2);
+                    
+                    
                     break;
                 case 3:
                     // Lógica para registrar defunción
+                    System.out.println("Ingrese año de la defuncion:");
+                    año = Integer.parseInt(lector.readLine());
+                    System.out.println("Ingrese mes de la defuncion:");
+                    mes = Integer.parseInt(lector.readLine());
+                    System.out.println("Ingrese dia de la defuncion:");
+                    dia = Integer.parseInt(lector.readLine());
+                    fechaDefuncion = LocalDate.of(año,mes,dia);
+                    
+                    System.out.println("Ingrese el rut del fallecido:");
+                    rutFallecido = lector.readLine();
+                    System.out.println("Ingrese la causa de la defuncion");
+                    Causa = lector.readLine();
+                    
+                    //sistema.registrarDefuncion(index, fechaDefuncion, Causa, rutFallecido);
+                    
                     break;
                 case 4:
                     // Lógica para emitir certificados
